87a5a96854d2819d8be794f757ffc42cbc0ede8f
QuadFlask <pop9310@gmail.com>
2019-09-24 22:00:49 +0900

    first commit

---
+++ test.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	rawLiteral := `test
+asdasd2
+asdasddd`
+
+	fmt.Println(rawLiteral)
+
+	rawLiteral = "123"
+
+	print("hello world")
+
+	print("hello world")
+}
